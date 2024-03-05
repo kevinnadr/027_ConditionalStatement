4a95d331a62440224bfdc70d500c2bb3fe57ed28
kevinnadr <kevin19305.ib@gmail.com>
2024-03-05 19:36:47 +0700

    Add project files.

---
+++ contoh bilangan.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main() {
+	int bilangan;
+	srand(time(0));
+
+	bilangan = rand() % 10;
+	cout << "nilai awalnya : " << bilangan << endl;
+
+	if (bilangan >= 5)
+	{
+		bilangan = 2 * bilangan;
+	}
+	cout << "nilai sekarang : " << bilangan;
+}
